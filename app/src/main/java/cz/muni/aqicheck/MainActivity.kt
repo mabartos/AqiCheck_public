a5bda2a6eb7eeb9f33971f478f9b4925778d4a65
Jan Maděra <maderajan@email.cz>
2022-02-19 16:42:16 +0100

    2. cviko - start

--- app/src/main/java/cz/muni/aqicheck/MainActivity.kt
+++ app/src/main/java/cz/muni/aqicheck/MainActivity.kt
@@ -2,18 +2,14 @@ package cz.muni.aqicheck
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.isVisible
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import cz.muni.aqicheck.databinding.ActivityMainBinding
 
-// TODO 1 Konstra UI
-// TODO 2 MainActivity -> layout, binding, bottom navigation
-// TODO 6 Resources
-
 class MainActivity : AppCompatActivity() {
 
     private val binding: ActivityMainBinding by lazy {
+        // TODO 2. BindingView
         ActivityMainBinding.inflate(layoutInflater)
     }
 
@@ -21,6 +17,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        // TODO 1. BottomNavigation + NavigationComponent
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
         binding.bottomNavigation.setupWithNavController(navController)
--- app/src/main/java/cz/muni/aqicheck/ui/favorites/FavoritesFragment.kt
+++ app/src/main/java/cz/muni/aqicheck/ui/favorites/FavoritesFragment.kt
@@ -1,5 +1,31 @@
 package cz.muni.aqicheck.ui.favorites
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import cz.muni.aqicheck.databinding.FragmentFavoritesBinding
 
-class FavoritesFragment: Fragment()
+class FavoritesFragment : Fragment() {
+
+    // TODO 11. FavoritesFragment binding
+    private lateinit var binding: FragmentFavoritesBinding
+
+    // TODO 12. onCreateView
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = FragmentFavoritesBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // TODO 14. (S) napsat funkci, která
+        // TODO - po klikud na tlačtíko -> button.setOnClickListener { }
+        // TODO - veme text z TextView
+        // TODO - Získá aktuální čas
+        // TODO - a spojí předchozí text se získaným časem
+        // System.currentTimeMillis()
+        // Calendar.getInstance()
+    }
+}
